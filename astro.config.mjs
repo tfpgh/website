bf22561b5c7e4530f0828297632d58086ed04e74
Toby Penner <tobypenner01@gmail.com>
2026-03-26 17:13:24 -0600

    Add homepage shell

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,20 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://tobypenner.com",
+  fonts: [
+    {
+      provider: fontProviders.fontshare(),
+      name: "Switzer",
+      cssVariable: "--font-heading",
+      weights: [400, 500, 600, 700, 800, 900],
+    },
+    {
+      provider: fontProviders.fontshare(),
+      name: "Sentient",
+      cssVariable: "--font-body",
+    },
+  ],
 });
